bede364aeed12597425f48c0d15b7a959381f06a
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-10 07:24:02 +0800

    2nd window

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import pandas as pd
 
 df = pd.read_csv('users.csv')
+lf = pd.read_csv('attendencecount.csv')
+
 
 
 
@@ -11,10 +13,27 @@ def click():
             if(entrypwd.get() == df.PASSWORD[j]):
                 secondwindow=tk.Tk()
                 secondwindow.minsize(800,600)
+                secondwindow.configure(bg='#b5faa0')
+                #studentname
+                studentname = tk.Label(secondwindow, text='Student Name',fg="#000000", bg="#b5faa0")
+                studententry = tk.Entry(secondwindow)
+                studentname.config(font=('Roboto' , 25))
+                studentname.grid(row=2,column=0, padx=1)
+                studententry.config(font=('Roboto' , 25))
+                studententry.grid(row=2,column=2, columnspan = 1, pady=5)
                 
+                #date
+                date = tk.Label(secondwindow, text='Date',fg="#000000", bg="#b5faa0")
+                dateentry = tk.Entry(secondwindow, width=20)
+                date.config(font=('Roboto' , 25))
+                date.grid(row=3,column=0, padx=1, pady=5)
+                dateentry.config(font=('Roboto' , 25))
+                dateentry.grid(row=3,column=2, columnspan = 1, pady=5)
+
+
+                
+
         
-
-
 window=tk.Tk()
 window.minsize(800,600)
 #window.maxsize(1000,600)
@@ -50,9 +69,6 @@ entrypwd.grid(row=3,column=2, columnspan = 1, pady=5)
 button1 = tk.Button(window, text = "Submit", bg='#FFFFFF', command=click)
 button1.grid(row=4,column=2, pady=30)
 button1.config(font=('Roboto' , 20))
-
-
-
 
 window.mainloop()
 
