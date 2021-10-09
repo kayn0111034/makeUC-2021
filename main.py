739072a7485538e2b9826a0a1db52b8aa5417403
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-10 05:41:13 +0800

    changes

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 import tkinter as tk
 import pandas as pd
 
+df = pd.read_csv('users.csv')
+
+def click():
+    for j in range(0,len(df)):
+        if(entryuser.get() == df.USERNAME[j]):
+            if(entrypwd.get() == df.PASSWORD[j]):
+                secondwindow=tk.Tk()
+                secondwindow.minsize(800,600)
+        
+
+
 window=tk.Tk()
-window.minsize(1200,600)
+window.minsize(800,600)
+#window.maxsize(1000,600)
 window.title("hello world")
 window.configure(bg='#b5faa0')
 
 #Label.config(font=('Roboto' , 25))
 
-teacher = tk.Label(window, text = "Teacher Login", width=40, height= 10, bg='#b5faa0')
-
-teacher.grid(row=1,column=1,padx=200,pady=200)
+teacher = tk.Label(window, text = "Teacher Login", bg='#b5faa0')
+teacher.config(font=('Roboto' , 25))
+teacher.grid(row=1,column=0,padx=15,pady=15)
 
 
 
 #username
-Label1 = tk.Label(window, text='Username' , height=3,fg="#FFFFFF", bg="#EA8B31")
+Label1 = tk.Label(window, text='Username',fg="#000000", bg="#b5faa0")
 entryuser = tk.Entry(window)
-Label1.config(font=('Roboto' , 35))
-Label1.grid(row=2,column=0, rowspan = 3, padx=175)
+Label1.config(font=('Roboto' , 25))
+Label1.grid(row=2,column=0, padx=1)
 entryuser.config(font=('Roboto' , 25))
 entryuser.grid(row=2,column=2, columnspan = 1, pady=5)
 
 
 
 #password
-Label2 = tk.Label(window, text='Username' , height=3,fg="#FFFFFF", bg="#EA8B31")
-entrypwd = tk.Entry(window)
-Label2.config(font=('Roboto' , 35))
-Label2.grid(row=3,column=0, rowspan = 3, padx=175)
+Label2 = tk.Label(window, text='Password',fg="#000000", bg="#b5faa0")
+entrypwd = tk.Entry(window, width=20)
+Label2.config(font=('Roboto' , 25))
+Label2.grid(row=3,column=0, padx=1, pady=5)
 entrypwd.config(font=('Roboto' , 25))
 entrypwd.grid(row=3,column=2, columnspan = 1, pady=5)
 
-button1 = Button(window, text = "Submit", width=40, height= 10, bg='#FFFFFF')
-button1.grid(row=4,column=1,padx=200,pady=200)
+button1 = tk.Button(window, text = "Submit", bg='#FFFFFF', command=click)
+button1.grid(row=4,column=2, pady=30)
+button1.config(font=('Roboto' , 20))
+
+
 window.mainloop()
+
+
+
