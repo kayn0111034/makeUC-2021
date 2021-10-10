e2376d325aad5b62455494fac2f36f9337747daa
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-10 08:05:05 +0800

    working secondwindow

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import tkinter as tk
 import pandas as pd
 
 df = pd.read_csv('users.csv')
-lf = pd.read_csv('attendencecount.csv')
-
+lf = pd.read_csv('attendance.csv')
 
 
 
@@ -11,24 +10,51 @@ def click():
     for j in range(0,len(df)):
         if(entryuser.get() == df.USERNAME[j]):
             if(entrypwd.get() == df.PASSWORD[j]):
+                def secondcheck():
+                    lf.loc[len(lf)] = [dateentry.get(), studententry.get(), comments.get()]
+                    lf.to_csv("attendance.csv", index=False)
+
+            
+                
                 secondwindow=tk.Tk()
                 secondwindow.minsize(800,600)
                 secondwindow.configure(bg='#b5faa0')
+
+                #attendance
+                attend = tk.Label(secondwindow, text = "Attendance", bg='#b5faa0')
+                attend.config(font=('Roboto' , 25))
+                attend.grid(row=1,column=1,padx=15,pady=50)
+
                 #studentname
                 studentname = tk.Label(secondwindow, text='Student Name',fg="#000000", bg="#b5faa0")
                 studententry = tk.Entry(secondwindow)
                 studentname.config(font=('Roboto' , 25))
-                studentname.grid(row=2,column=0, padx=1)
+                studentname.grid(row=2,column=0, padx=50,pady=25)
                 studententry.config(font=('Roboto' , 25))
-                studententry.grid(row=2,column=2, columnspan = 1, pady=5)
+                studententry.grid(row=2,column=1, columnspan = 1, pady=5)
                 
                 #date
                 date = tk.Label(secondwindow, text='Date',fg="#000000", bg="#b5faa0")
                 dateentry = tk.Entry(secondwindow, width=20)
                 date.config(font=('Roboto' , 25))
-                date.grid(row=3,column=0, padx=1, pady=5)
+                date.grid(row=3,column=0, padx=1, pady=30)
                 dateentry.config(font=('Roboto' , 25))
-                dateentry.grid(row=3,column=2, columnspan = 1, pady=5)
+                dateentry.grid(row=3,column=1, columnspan = 1, pady=5)
+
+                #comment
+                stdcomment = tk.Label(secondwindow, text='Comment',fg="#000000", bg="#b5faa0")
+                comments = tk.Entry(secondwindow, width=20)
+                stdcomment.config(font=('Roboto' , 25))
+                stdcomment.grid(row=4,column=0, padx=1, pady=30)
+                comments.config(font=('Roboto' , 25))
+                comments.grid(row=4,column=1, columnspan = 1, pady=5)
+
+                #save button
+                savebtn = tk.Button(secondwindow, text = "Save", bg='#FFFFFF', command=secondcheck)
+                savebtn.grid(row=5,column=1, pady=30)
+                savebtn.config(font=('Roboto' , 20))
+
+                
 
 
                 
@@ -44,7 +70,7 @@ window.configure(bg='#b5faa0')
 
 teacher = tk.Label(window, text = "Teacher Login", bg='#b5faa0')
 teacher.config(font=('Roboto' , 25))
-teacher.grid(row=1,column=0,padx=15,pady=15)
+teacher.grid(row=1,column=1,padx=15,pady=50)
 
 
 
@@ -52,9 +78,9 @@ teacher.grid(row=1,column=0,padx=15,pady=15)
 Label1 = tk.Label(window, text='Username',fg="#000000", bg="#b5faa0")
 entryuser = tk.Entry(window)
 Label1.config(font=('Roboto' , 25))
-Label1.grid(row=2,column=0, padx=1)
+Label1.grid(row=2,column=0, padx=50,pady=20)
 entryuser.config(font=('Roboto' , 25))
-entryuser.grid(row=2,column=2, columnspan = 1, pady=5)
+entryuser.grid(row=2,column=1, columnspan = 1, pady=5)
 
 
 
@@ -62,12 +88,13 @@ entryuser.grid(row=2,column=2, columnspan = 1, pady=5)
 Label2 = tk.Label(window, text='Password',fg="#000000", bg="#b5faa0")
 entrypwd = tk.Entry(window, width=20)
 Label2.config(font=('Roboto' , 25))
-Label2.grid(row=3,column=0, padx=1, pady=5)
+Label2.grid(row=3,column=0, padx=50, pady=30)
 entrypwd.config(font=('Roboto' , 25))
-entrypwd.grid(row=3,column=2, columnspan = 1, pady=5)
+entrypwd.grid(row=3,column=1, columnspan = 1, pady=5)
 
+#submit
 button1 = tk.Button(window, text = "Submit", bg='#FFFFFF', command=click)
-button1.grid(row=4,column=2, pady=30)
+button1.grid(row=4,column=1, pady=30)
 button1.config(font=('Roboto' , 20))
 
 window.mainloop()
